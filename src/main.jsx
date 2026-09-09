e00a2bfb2b1f043e81265ea47813992328933904
rsemsaco <maggessiricardo@usp.br>
2026-09-09 01:04:15 -0300

    Load final layout refinement layer

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,7 @@ import './styles/experience.css'
 import './styles/psychology.css'
 import './styles/data.css'
 import './styles/premium.css'
+import './styles/refinements.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
